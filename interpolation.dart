7603aad4133b1c765707bacc41f28294a64860a8
ianwambaire <ian.wambaire@strathmore.edu>
2025-06-13 21:56:43 +0300

    string interpolation

---
+++ interpolation.dart
@@ -0,0 +1,10 @@
+void main() {
+  String student;
+  int admission;
+  int age;
+  student = "Ian";
+  admission = 159799;
+  age = 21;
+  print(
+      "The student's name is: $student and the admission is $admission and the age is ${age + 2}");
+}
