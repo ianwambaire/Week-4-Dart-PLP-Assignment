6466004c03b6dcd9c5ed50b65fb89de0be92be65
ianwambaire <ian.wambaire@strathmore.edu>
2025-06-13 21:51:28 +0300

    string concatenation

--- main.dart
+++ main.dart
@@ -1,3 +1,10 @@
-void main(){
-  
+void main() {
+  //concatenation
+  String fname;
+  String lname;
+  String fullName;
+  fname = "Ian";
+  lname = "Wambaire";
+  fullName = fname + " " + lname;
+  print(fullName);
 }
